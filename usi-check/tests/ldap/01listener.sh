7b5578c6252538dfbd6521118e10b873fc98478a
Nico Stöckigt <stoeckigt@univention.de>
2016-06-09 10:16:51 +0000

    Ticket: #2016060821000334 - check for listener bad_cache
    git-svn-id: svn+ssh://svn.knut.univention.de/var/univention/svn/dev/trunk/internal/univention-support-info@70019 1b283449-9f2f-0410-b571-8eb228e1a901

--- usi-check/tests/ldap/01listener.sh
+++ usi-check/tests/ldap/01listener.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-nID=$(cat $1/info/notifierID)
-lID=$(cat $1/info/listenerID)
+nID=$( cat $1/info/notifierID )
+lID=$( cat $1/info/listenerID )
+lBC=$( [ -f $1/files/var_lib_univention-directory-listener_bad_cache ]; echo $? )
 result=""
 exitcode=0
 
@@ -9,8 +10,6 @@ function quit() {
 	echo -e $result
 	exit $exitcode
 }
-
-
 
 
 
@@ -22,17 +21,21 @@ if [ -z $lID ]; then
 	result="${result}No listener ID\n"
 	exitcode=1
 fi
-if [ $exitcode -ne 0 ]; then
+if [ $lBC -eq 0 ]; then
+	result="${result}Listener Cache seems brocken [/var/lib/univention-directory-listener/bad_cache is present!]\n"
+	exitcode=3
+fi
+if [ $exitcode -eq 1 ]; then #skip ID check when one or both IDs not found
 	quit
 fi
 
 
 if [ $nID -ne $lID ]; then
-	result="Listener (${lID}) and Notified ID (${nID}) not equal!"
+	result="${result}Listener (${lID}) and Notified ID (${nID}) not equal!"
 	exitcode=1
 else
-	result="Listener and Notified ID okay"
-	exitcode=0
+	result="${result}Listener and Notified ID okay"
+#	exitcode=0 #initially set to 0; exitcode is 3 when bad_cache is detected
 fi
 
 quit
