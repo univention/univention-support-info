43edaee61bf8ed10ed0df4dad9673ad33e677edb
Christina Scheinig <scheinig@univention.de>
2016-08-22 14:24:12 +0000

    Bug #41523 03servfail.py checks servfail in daemon log. 03segfault.py now without daemon.log-check
    git-svn-id: svn+ssh://svn.knut.univention.de/var/univention/svn/dev/trunk/internal/univention-support-info@71805 1b283449-9f2f-0410-b571-8eb228e1a901

--- usi-check/tests/general/03segfaults.py
+++ usi-check/tests/general/03segfaults.py
@@ -17,16 +17,6 @@ def segfaults_in_syslog(syslog_path, search_string):
 	return count
 
 
-def segfaults_in_daemon(daemon_path, search_string):
-#		find segfaults in daemonlog 
-	count = 0
-	with open(daemon_path) as infile:
-		for line in infile:
-			m = search_string.match(line)
-			if m:
-				count += 1
-	return count
-
 def get_logfiles(filename):
 	#logfiles einsammeln
 	usi_path = sys.argv[1]
@@ -43,18 +33,6 @@ def logfile_count(search_string):
 			if syslog_count > 0:
 				print('Syslogfile %s enthaelt %s segfaults' % (logfile,syslog_count))
 				status += 1
-		except IOError as exc:
-			print(repr(exc))
-			print('Error opening var_log_syslog file %s: %s' % (syslog_path, exc))
-			return -1
-	daemonlogfiles = get_logfiles('var_log_daemon_*')
-	for logfile in daemonlogfiles:
-		print (logfile)
-		try:
-			daemon_count = segfaults_in_daemon(logfiles, search_string)
-			if daemon_count > 0:
-				print('Daemon.log %s enthaelt %s segfaults' % (logfile, daemon_count))
-				status += 2
 		except IOError as exc:
 			print(repr(exc))
 			print('Error opening var_log_syslog file %s: %s' % (syslog_path, exc))
---
+++ usi-check/tests/general/03servfail.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import sys
+import os
+import re 
+from ucr import UCR
+from glob import glob
+
+
+def servfail_in_daemon(daemon_path, search_daemon_string):
+#		find segfaults in daemonlog 
+	count = 0
+	with open(daemon_path) as infile:
+		for line in infile:
+			m = search_daemon_string.match(line)
+			if m:
+				count += 1
+	return count
+
+def get_logfiles(filename):
+	#logfiles einsammeln
+	usi_path = sys.argv[1]
+	path = os.path.join(usi_path, 'files')
+	return glob(path + '/' + filename)
+
+def logfile_count(search_daemon_string):
+	status = 0
+	daemonlogfiles = get_logfiles('var_log_daemon.log_*')
+	for logfile in daemonlogfiles:
+		print (logfile)
+		try:
+			daemon_count = servfail_in_daemon(logfile, search_daemon_string)
+			if daemon_count > 0:
+				print('Daemon.log %s enthaelt %s SERVFAILS' % (logfile, daemon_count))
+				status += 2
+		except IOError as exc:
+			print(repr(exc))
+			print('Error opening var_log_syslog file %s: %s' % (syslog_path, exc))
+			return -1
+	return status
+	
+def main():
+	usi_path = sys.argv[1]
+	search_daemon_string = re.compile(r'.*unexpected\ rcode\ \(SERVFAIL\).*',re.I)
+	return logfile_count(search_daemon_string)
+
+
+exit_status = main()
+sys.exit(exit_status)
+
+# vim: set ts=4 sw=4 tw=0 noet :
