049eb2f92fd0b5cf6526c784d4827cd92860d782
Nico Stöckigt <stoeckigt@univention.de>
2017-10-02 15:02:11 +0000

    Bug #45476
    git-svn-id: svn+ssh://svn.knut.univention.de/var/univention/svn/dev/trunk/internal/univention-support-info@82765 1b283449-9f2f-0410-b571-8eb228e1a901

--- usi-check/tests/ldap/02permissions.sh
+++ usi-check/tests/ldap/02permissions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-#FIXME: I think this is broken. There are many setups where license check does not work (DC-Slaves, UCS@school, Memberservers ... ... )
+#  FIXME: I think this is broken. There are many setups where license check does not work (DC-Slaves, UCS@school, Memberservers ... ... )
+## This is a problem with the Script itself (univention-lecense-check) sie also Bug#45476
+
 if [ "$(grep "Permission denied" $1/info/univention-license-check)" ];
 then
 	result="Wrong LDAP permissions for machine account!"
