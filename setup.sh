bd9121ccf1aa19004bc0c15ba47eea3f18d3f4f7
Nico Stöckigt <stoeckigt@univention.de>
2018-04-05 09:09:10 +0000

    Bug #46781
    git-svn-id: svn+ssh://svn.knut.univention.de/var/univention/svn/dev/trunk/internal/univention-support-info@82986 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SVN_DIR=$( pwd )
+
+# add path to environment PATH
+if ! grep -Exq "PATH=.*(~|$HOME)/SVN/internal/univention-support-info/usi-check.*" "$HOME/.bash_local"; then
+	echo "PATH=\$PATH:~/SVN/internal/univention-support-info/usi-check" >>"$HOME/.bash_local"
+fi
+if ! grep -xq "\[ -f ~/.bash_local \] && . ~/.bash_local" "$HOME/.bashrc"; then
+	echo -e "[ -f ~/.bash_local ] && . ~/.bash_local" >>"$HOME/.bashrc"
+fi
+
+# write new and old password to password file
+echo -ne "dt9jdvffTHMPjohFpYfy\nx53Ay2Fgc0Pn" >"${HOME}/.gnupg/support.pw"
