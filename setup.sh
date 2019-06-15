1713ab98c1b52d6b73e210532532143d52b1499e
Nico Stöckigt <stoecki@univention.de>
2019-06-15 03:31:39 +0200

    changed frm svn to git

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 SVN_DIR=$( pwd )
 
 # add path to environment PATH
-if ! grep -Exq "PATH=.*(~|$HOME)/SVN/internal/univention-support-info/usi-check.*" "$HOME/.bash_local"; then
-	echo "PATH=\$PATH:~/SVN/internal/univention-support-info/usi-check" >>"$HOME/.bash_local"
+if ! grep -Exq "PATH=.*(~|$HOME)/GIT/univention-support-info/usi-check.*" "$HOME/.bash_local"; then
+	echo "PATH=\$PATH:~/GIT/univention-support-info/usi-check" >>"$HOME/.bash_local"
 fi
 if ! grep -xq "\[ -f ~/.bash_local \] && . ~/.bash_local" "$HOME/.bashrc"; then
 	echo -e "[ -f ~/.bash_local ] && . ~/.bash_local" >>"$HOME/.bashrc"
