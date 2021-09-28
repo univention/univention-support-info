9c2b481bc218265350aa078f57a7f1e18739599f
Philipp Hahn <hahn@univention.de>
2021-09-28 09:35:17 +0200

    ci: Make version depend on time
    from last commit for reproducible builds.
    Fall back to now.

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+from __future__ import absolute_import
+
+from datetime import datetime
+from os import environ
+
 import setuptools
 
-setuptools.setup()
+try:
+    ts = environ["CI_COMMIT_TIMESTAMP"]
+    dt = datetime.fromisoformat(ts)
+except LookupError:
+    dt = datetime.now()
+VERSION = "0.{0:%Y%m%d}.{0:%H%M%S}".format(dt)
+
+setuptools.setup(version=VERSION)
